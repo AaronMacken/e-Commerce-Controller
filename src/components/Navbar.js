0a443d4dd62dffd43b5e6ce3aaa3815b8f05da06
Aaron Macken <aaron.a.macken@gmail.com>
2019-12-25 15:37:41 -0800

    Create jsf for login page using material ui template

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,15 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-// import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-// import Badge from '@material-ui/core/Badge';
-// import Container from '@material-ui/core/Container';
-// import Grid from '@material-ui/core/Grid';
-// import Paper from '@material-ui/core/Paper';
-// import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-// import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
-// import Chart from './Chart';
-// import Deposits from './Deposits';
-// import Orders from './Orders';
 
 
 const drawerWidth = 240;
@@ -114,7 +104,6 @@ export default function Dashboard() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  // const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return [
       <CssBaseline />,
@@ -132,7 +121,7 @@ export default function Dashboard() {
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title} >
             Hippie Headquarters
           </Typography>
-          <Button color="secondary" variant="contained">Login</Button>
+          <Button color="secondary" variant="contained">Logout</Button>
         </Toolbar>
       </AppBar>,
       
@@ -153,8 +142,5 @@ export default function Dashboard() {
         <Divider />
         <List>{secondaryListItems}</List>
       </Drawer>
-  
-      
-   
   ];
 }
