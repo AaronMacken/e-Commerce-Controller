6631edd95eec12e964188b35069d0f5aa15f8c4f
Aaron Macken <aaron.a.macken@gmail.com>
2019-12-25 19:29:12 -0800

    Add title styling to pages

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,11 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { makeStyles } from "@material-ui/core/styles";
+import * as Yup from "yup";
+import Navbar from "../components/Navbar";
+import { Grid, Paper, Input, Typography, Container } from "@material-ui/core";
 
-export default class ProductForm extends Component {
-    render() {
-        return (
-            <div>
-                Product Form Page
-            </div>
-        )
-    }
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+    backgroundColor: "#c0cbff40"
+  },
+  titleWrapper: {
+    padding: theme.spacing(3)
+  },
+  title: {
+    marginBottom: theme.spacing(1)
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  gridItem: {
+    margin: "auto"
+  }
+}));
+
+export default function ProductForm() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <div className={classes.titleWrapper}>
+          <Typography
+            component="h1"
+            variant="h4"
+            color="textPrimary"
+            align="center"
+            className={classes.title}
+          >
+            New Product
+          </Typography>
+          <Typography
+            display="block"
+            variant="p"
+            color="textSecondary"
+            align="center"
+          >
+            Add item to online inventory
+          </Typography>
+        </div>
+        <Container maxWidth="lg" className={classes.container}>
+         <Paper>
+             Form here
+         </Paper>
+        </Container>
+      </main>
+    </div>
+  );
 }
--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,81 +1,107 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Grid } from '@material-ui/core';
-import { Paper } from '@material-ui/core';
-import { Input } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
-import ProductItem from './ProductItem';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Grid, Paper, Input, Typography } from "@material-ui/core";
+import ProductItem from "./ProductItem";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex'
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-        backgroundColor: "#c0cbff40"
-    },
-    title: {
-        fontSize: '10rem'
-    },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4)
-    },
-    gridItem: {
-        margin: 'auto'
-    },
-    searchBar: {
-        width: '100%'
-    }
+  root: {
+    display: "flex"
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+    backgroundColor: "#c0cbff40"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  titleWrapper: {
+    padding: theme.spacing(3)
+  },
+  title: {
+    marginBottom: theme.spacing(1)
+  },
+  searchBar: {
+    width: "100%"
+  },
+  gridItem: {
+    margin: "auto"
+  }
 }));
 
-
 export default function Products() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Navbar></Navbar>
-            <main className={classes.content}>
-                {/* AppBarSpacer div class for top margin */}
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="lg" className={classes.container}>
-
-                    <Grid container spacing={3}>
-
-                        {/* grid items */}
-                        <Grid item xs={12}>
-                            <Input placeholder="Search Products" className={classes.searchBar}></Input>
-                        </Grid>
-
-
-                        <Grid item xs={12} className={classes.gridItem}>
-                            <Paper>
-                                <ProductItem img="https://hemp-xr.com/wp-content/uploads/2019/12/hemp-xr-oil.jpg"
-                                price={14.99} title="CBD Oil Product 1"/>
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={12} className={classes.gridItem}>
-                            <Paper>
-                                <ProductItem img="https://hemp-xr.com/wp-content/uploads/2019/12/hemp-xr-oil.jpg"
-                                price={20.99} title="CBD Oil Product 2"/>
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={12} className={classes.gridItem}>
-                            <Paper>
-                                <ProductItem img="https://hemp-xr.com/wp-content/uploads/2019/12/hemp-xr-oil.jpg"
-                                price={4.99} title="CBD Oil Product 3"/>
-                            </Paper>
-                        </Grid>
-                        
-                    </Grid>
-                </Container>
-            </main>
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Navbar></Navbar>
+      <main className={classes.content}>
+        {/* AppBarSpacer div class for top margin */}
+        <div className={classes.appBarSpacer} />
+        <div className={classes.titleWrapper}>
+          <Typography
+            component="h1"
+            variant="h4"
+            color="textPrimary"
+            align="center"
+            className={classes.title}
+          >
+            Products
+          </Typography>
+          <Typography
+            display="block"
+            variant="p"
+            color="textSecondary"
+            align="center"
+          >
+            See products, edit & delete
+          </Typography>
         </div>
-    )
-}
 
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            {/* grid items */}
+            <Grid item xs={12}>
+              <Input
+                placeholder="Search Products"
+                className={classes.searchBar}
+              ></Input>
+            </Grid>
+
+            <Grid item xs={12} className={classes.gridItem}>
+              <Paper>
+                <ProductItem
+                  img="https://hemp-xr.com/wp-content/uploads/2019/12/hemp-xr-oil.jpg"
+                  price={14.99}
+                  title="CBD Oil Product 1"
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} className={classes.gridItem}>
+              <Paper>
+                <ProductItem
+                  img="https://hemp-xr.com/wp-content/uploads/2019/12/hemp-xr-oil.jpg"
+                  price={20.99}
+                  title="CBD Oil Product 2"
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} className={classes.gridItem}>
+              <Paper>
+                <ProductItem
+                  img="https://hemp-xr.com/wp-content/uploads/2019/12/hemp-xr-oil.jpg"
+                  price={4.99}
+                  title="CBD Oil Product 3"
+                />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </div>
+  );
+}
